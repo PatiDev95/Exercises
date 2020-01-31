263c4de20c37b4e8ecd5f9c16a81c46cf9c4aa49
patidev95 <kubinskapatrycja@gmail.com>
2020-01-31 15:05:21 +0100

    Array 2x2 exercises

--- KUBIAK/TabliceKolekcje.cs
+++ KUBIAK/TabliceKolekcje.cs
@@ -73,7 +73,7 @@ namespace KUBIAK
 
         public static void Zadanie44()
         {
-            int n = 10, i, j;
+            int n = 10, i, j, suma;
             int[,] macierz = new int[n, n];
 
             Console.WriteLine("Program wyświetla zawartość tablicy dwuwymiarowej " + n + "x" + n + "-elementowej.");
@@ -90,6 +90,16 @@ namespace KUBIAK
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine();
+
+            suma = 0;
+
+            for (i = 0; i < n; i++)
+            {
+                suma = suma + macierz[i, i];
+            }
+
+            Console.WriteLine("Suma wyróżnionych elementów w tablicy wynosi " + suma + ".");
             Console.WriteLine();
         }
 
@@ -117,11 +127,106 @@ namespace KUBIAK
             suma = 0;
             for (i = 0; i < n; i++)
             {
-                suma = suma + macierz[i, i];
+                suma = suma + macierz[i, n-i-1];
             }
-
             Console.WriteLine("Suma wyróżnionych elementów w tablicy wynosi " + suma + ".");
             Console.WriteLine();
+        }
+        public static void Zadanie46()
+        {
+            int n = 10, i, j, suma;
+            int[,] macierz = new int[n, n];
+
+            Console.WriteLine("Program wyświetla zawartość tablicy dwuwymiarowej " + n + "x" + n + "-elementowej.");
+            
+            for (i=0; i<n; i++)
+            {
+                for(j=0; j<n; j++)
+                {
+                    if (n == i + j + 1)
+                        macierz[i, j] = i;
+                    else
+                        macierz[i, j] = 0;
+
+                    Console.Write(macierz[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            suma = 0;
+
+            for (i=0;i<n;i++)
+            {
+                suma = suma + macierz[i, n-i-1];
+            }
+            Console.WriteLine("Suma wyróżnionych elementów tablicy wynosi " + suma + ".");
+            Console.WriteLine();
+        }
+
+        public static void Zadanie47()
+        {
+            int n = 10, i, j, suma1, suma2;
+            int[,] tablica = new int[n, n];
+
+            Console.WriteLine("Program wyświetla zawartość tablicy dwuwymiarowej " + n + "x" + n + "-elementowej.");
+
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    if (j == 0) tablica[i, j] = i;
+                    if (j == 1) tablica[i, j] = i * i;
+                    if (j > 1) tablica[i, j] = 0;
+
+                    Console.Write(tablica[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            suma1 = 0;
+            suma2 = 0;
+
+            for(i=0;i<n;i++)
+            {
+                suma1 = suma1 + tablica[i, 0];
+                suma2 = suma2 + tablica[i, 1];
+            }
+            Console.WriteLine("Suma liczb pierwszej kolumny to: " + suma1 + " a suma liczb drugiej kolumny to: " + suma2 + ".");
+            Console.WriteLine();
+        }
+
+        public static void Zadanie48()
+        {
+            int n = 10, i, j;
+            int[,] a = new int[n, n];
+            int[,] b = new int[n, n];
+
+            Console.WriteLine("Program wyświetla tablice a.");
+
+            for (i = 0; i < n; i++)
+            {
+                for(j=0;j<n;j++)
+                {
+                    a[i, j] = j;
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Program wyświetla tablicę b, z wartościami z tablicy a i zamianą kolumny na wiersze.");
+
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    b[i, j] = a[j, i];
+                    Console.Write(b[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+           
         }
     }
 }
